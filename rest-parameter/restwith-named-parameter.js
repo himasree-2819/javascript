d63ed52539664bade98fe925d77ed83f491cd7ad
himasree-2819 <himamanoj19@gmail.com>
2025-09-10 10:58:03 +0530

    commit

---
+++ rest-parameter/restwith-named-parameter.js
@@ -0,0 +1,6 @@
+function greet(firstname , lastname,...titles){
+    console.log(`name: ${firstname} ${lastname}`);
+    console.log(`titles: ${titles.join(',')}`);
+        
+}
+greet("hima","k","Bcom","mernstack")
---
+++ spread-operators/object/merging-object.js
@@ -0,0 +1,5 @@
+const obj1 = {name:'apple'};
+const obj2 = {color:'Red', taste:'sweet'};
+const merged = {...obj1,...obj2};
+console.log(merged);
+
